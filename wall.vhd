5a402b49072e3715b8ad5800f66af9cefafb7f34
tangshh718 <tangshh718@gmail.com>
2025-12-01 14:42:55 -0500

    Update wall.vhd

--- wall.vhd
+++ wall.vhd
@@ -3,36 +3,34 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity wall_field is
-  generic(
-    GRID_W : integer := 40;
-    GRID_H : integer := 30
-  );
-  port(
-    cell_x  : in  unsigned(5 downto 0); -- 0..GRID_W-1
-    cell_y  : in  unsigned(5 downto 0); -- 0..GRID_H-1
-    is_wall : out std_logic
-  );
+    generic (
+        GRID_WIDTH  : integer := 40;
+        GRID_HEIGHT : integer := 30
+    );
+    port(
+        x       : in  integer range 0 to GRID_WIDTH-1;
+        y       : in  integer range 0 to GRID_HEIGHT-1;
+        is_wall : out std_logic
+    );
 end entity;
 
 architecture rtl of wall_field is
 begin
-  process(cell_x, cell_y)
-    variable w : std_logic := '0';
-  begin
-    if (cell_x = 0) or (cell_y = 0) or
-       (cell_x = to_unsigned(GRID_W-1, cell_x'length)) or
-       (cell_y = to_unsigned(GRID_H-1, cell_y'length)) then
-      w := '1';
-    else
--- the inside walls can be changed
-      if (cell_x = to_unsigned(10,6) and cell_y >  to_unsigned(4,6) and cell_y < to_unsigned(GRID_H-5,6) and cell_y /= to_unsigned(12,6)) or
-         (cell_x = to_unsigned(20,6) and cell_y >  to_unsigned(2,6) and cell_y < to_unsigned(GRID_H-7,6) and cell_y /= to_unsigned(18,6)) or
-         (cell_x = to_unsigned(30,6) and cell_y >  to_unsigned(6,6) and cell_y < to_unsigned(GRID_H-3,6) and cell_y /= to_unsigned(9,6)) then
-        w := '1';
-      else
-        w := '0';
-      end if;
-    end if;
-    is_wall <= w;
-  end process;
+    process(x, y)
+    begin
+        is_wall <= '0';
+
+        -- 外框
+        if (x = 0) or (y = 0) or
+           (x = GRID_WIDTH-1) or (y = GRID_HEIGHT-1) then
+            is_wall <= '1';
+
+        -- 内部几条竖墙（和我们之前 UART 版本类似）
+        elsif (x = 10 and y > 4  and y < GRID_HEIGHT-5 and y /= 12) or
+              (x = 20 and y > 2  and y < GRID_HEIGHT-7 and y /= 18) or
+              (x = 30 and y > 6  and y < GRID_HEIGHT-3 and y /=  9) then
+            is_wall <= '1';
+        end if;
+    end process;
 end architecture;
+
